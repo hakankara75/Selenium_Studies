428b50ac37681d876f160637353857102d0778d6
HakanKara75 <hakanbatirhan@gmail.com>
2023-04-16 22:02:33 +0300

    v

--- src/test/java/day12/C02.java
+++ src/test/java/day12/C02.java
@@ -1,52 +1,42 @@
 package day12;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import utilities.TestBase;
 
 import static org.junit.Assert.assertTrue;
 
-public class C01 extends TestBase {
+public class C02 extends TestBase {
     /*
     2. Navigate to url 'http://automationexercise.com'
 3. Verify that home page is visible successfully
 4. Scroll down page to bottom
 5. Verify 'SUBSCRIPTION' is visible
-6. Click on arrow at bottom right side to move upward
+6. Scroll up page to top
 7. Verify that page is scrolled up and 'Full-Fledged practice website for Automation Engineers' text is visible on screen
      */
     @Test
-    public void test() throws InterruptedException {
+    public void test(){
+
 //        2. Navigate to url 'http://automationexercise.com'
         driver.get("http://automationexercise.com");
 
 //        3. Verify that home page is visible successfully
-        WebElement homePage = driver.findElement(By.xpath("//html"));
+        WebElement homePage= findXpathWebelement("//html");
         assertTrue(homePage.isDisplayed());
-
-        //try-catch alttan açılıp kapanan reklamı kapatmak icin shadow root (closed)
-        try {
-            driver.findElement(By.xpath("//div[@class='grippy-host']")).click();
-            driver.findElement(By.xpath("//path[@stroke='#FAFAFA']")).click();
-            Thread.sleep(2000);
-        }catch (Exception s){
-
-        }
-
 
 //        4. Scroll down page to bottom
         Actions actions = new Actions(driver);
         WebElement subscription = driver.findElement(By.xpath("//h2[text()='Subscription']"));
         actions.scrollToElement(subscription).perform();
+        threadSleep(2);
 
 //        5. Verify 'SUBSCRIPTION' is visible
-        assertTrue(subscription.isDisplayed());
+       assertTrue(subscription.isDisplayed());
 
-//        6. Click on arrow at bottom right side to move upward
+//        6. Scroll up page to top
         //try-catch alttan açılıp kapanan reklamı kapatmak icin shadow root (closed)
         try {
             driver.findElement(By.xpath("//div[@class='grippy-host']")).click();
@@ -58,9 +48,7 @@ public class C01 extends TestBase {
         driver.findElement(By.xpath("//a[@id='scrollUp']")).click();
 
 //        7. Verify that page is scrolled up and 'Full-Fledged practice website for Automation Engineers' text is visible on screen
-
-        WebElement text = driver.findElement(By.xpath("//h2[text()='Full-Fledged practice website for Automation Engineers']"));
-        assertTrue(text.isDisplayed());
-
+        WebElement verifyText= findXpathWebelement("(//h2[text()='Full-Fledged practice website for Automation Engineers'])[1]");
+        assertTrue(verifyText.isDisplayed());
     }
 }
